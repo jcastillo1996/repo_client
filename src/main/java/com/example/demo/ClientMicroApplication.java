d52e8edc37afa3dacd87b7b2f745ee8f4720e7cd
jc19961203 <38443191+jc19961203@users.noreply.github.com>
2020-07-21 22:39:24 -0500

    configurando Eureka Client

--- src/main/java/com/example/demo/ClientMicroApplication.java
+++ src/main/java/com/example/demo/ClientMicroApplication.java
@@ -2,8 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 //import org.springframework.boot.autoconfigure.web.reactive.error.ErrorWebFluxAutoConfiguration;
-
+@EnableEurekaClient
 @SpringBootApplication
 //(exclude = ErrorWebFluxAutoConfiguration.class)
 public class ClientMicroApplication {
